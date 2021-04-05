25972718648ae59247750c8c7e0a810020887c8b
Jess <jesskaria1@gmail.com>
2021-04-05 12:58:19 +0100

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="navbar-menu">
         <div className="navbar-start">
           <div className="navbar-item">
-            <h1 id="footer-createdby" className="subtitle is-5">created by jess karia</h1>
+            <h1 id="footer-createdby" className="subtitle is-5">created by Jess Karia.</h1>
           </div>
         </div>
         <div className="navbar-end">
